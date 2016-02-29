c2e645492a8fa0783638d5445e55b3beced3d1b8
Edward Hu <hues@usc.edu>
2016-02-28 19:12:17 -0800

    server port

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var app = express();
-app.use(express.static(__dirname+'/public'))
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+var port = process.env.PORT || 3000;
+app.use(express.static(__dirname+'/public'));
+var server = app.listen(port, function() {
+	var host = server.address().address;
+	var port = server.address().port;
+	console.log('Example app listening at http://%s:%s', host, port);
 });
